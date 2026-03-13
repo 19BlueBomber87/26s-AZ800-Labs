80384515a09450c32a4895897c66056233e912d5
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2026-03-13 09:41:01 -0800

    Update GoldenImage.ps1

--- HyperV-QuickCreate-GoldenImages/GoldenImage.ps1
+++ HyperV-QuickCreate-GoldenImages/GoldenImage.ps1
@@ -5,6 +5,10 @@
 # Purpose:  Prepare golden images via SysPrep
 # Location: Anchorage, Alaska lab environment
 # =============================================================================
+# =========================================================================================================================================================
+# Use link below to get 'New-Lab_VM' command
+# https://github.com/19BlueBomber87/26s-AZ800-Labs/blob/main/HyperV-QuickCreate-GoldenImages/AZ-800Celebrate-HyperV%20Creation%20and%20Disk%20Functions.ps1
+# =========================================================================================================================================================
 New-Lab_VM -VMNames GoldenImage-ServerCore -HyperVSwitch EXT-INT -ISOPath C:\ISO\20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso
 New-Lab_VM -VMNames GoldenImage-DesktopExperience -HyperVSwitch EXT-INT -ISOPath C:\ISO\20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso
 
