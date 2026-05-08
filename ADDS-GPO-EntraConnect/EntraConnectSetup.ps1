ae3b1113d50b25e813f0cd7c232cd9618c7d9790
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2026-05-08 14:45:01 -0800

    Update EntraConnectSetup.ps1

--- ADDS-GPO-EntraConnect/EntraConnectSetup.ps1
+++ ADDS-GPO-EntraConnect/EntraConnectSetup.ps1
@@ -47,10 +47,9 @@
 # There's no method to revert the result of a one-way function to the plain text version of a password.
 
 # File Hash Test illustrate how hashing works
-"yahoo" | Out-File .\yahoo.txt
+"Yahoo" | Out-File .\yahoo.txt
 Get-Content .\yahoo.txt
 Get-FileHash .\yahoo.txt -Algorithm SHA256
-
 
 # Format-Hex .\yahoo.txt
 
@@ -60,9 +59,19 @@ Get-FileHash .\yahoo.txt -Algorithm SHA256
 
 # 00000000   FF FE 59 00 61 00 68 00 6F 00 6F 00 0D 00 0A 00  .þY.a.h.o.o.....
 
+
 # Moose in Hex
 # 4D 00 6F 00 6F 00 73 00 65 00
+"Moose" | Out-File .\yahoo.txt
+Get-Content .\yahoo.txt
+Get-FileHash .\yahoo.txt -Algorithm SHA256
+Format-Hex .\yahoo.txt
 
+#            Path: C:\Users\A19mk\yahoo.txt
+
+#            00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F
+
+# 00000000   FF FE 4D 00 6F 00 6F 00 73 00 65 00 0D 00 0A 00  .þM.o.o.s.e.....
 
 
 # Explanation of Binary
