3733c5bb8a15cda6f8c7bcd3fb2a7ae5c1d89fcc
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2026-05-14 09:08:17 -0800

    Update Install AD DS - Promote DCs - Forest Trust.ps1

--- ADDS-GPO-EntraConnect-LinuxADJoin/Install AD DS - Promote DCs - Forest Trust.ps1
+++ ADDS-GPO-EntraConnect-LinuxADJoin/Install AD DS - Promote DCs - Forest Trust.ps1
@@ -143,7 +143,8 @@ New-ADReplicationSite -Name Nome -Description "Nome Office AD DS Site" -Verbose 
 New-ADReplicationSite -Name EagleRiver -Description "Nome Office AD DS Site" -Verbose *>&1
 New-ADReplicationSubnet -Name "192.168.88.0/24" -Site Nome -Location "Nome Office" -Verbose *>&1
 New-ADReplicationSubnet -Name "192.168.100.0/24" -Site EagleRiver -Location "Eagle River Office" -Verbose *>&1
-Set-ADReplicationSiteLink -Identity "DEFAULTIPSITELINK" -SitesIncluded @{Add="Nome","EagleRiver"} -Verbose *>&1
+$sites = "Nome","EagleRiver"
+Set-ADReplicationSiteLink -Identity "DEFAULTIPSITELINK" -SitesIncluded @{Add=$sites} -Verbose *>&1
 
 Add-DnsServerConditionalForwarderZone -Name "moosewyre.fun" -MasterServers 192.168.88.8 -Verbose *>&1 
 # Note: A tree domain (new domain with a non-contiguous namespace, e.g., moosewyre.fun in a forest whose root is something like corp.local) creates a separate DNS namespace inside the same forest.
